01e0263a921c90b47d2ec3f23d4ffc1f09dd8faf
axel paulander <axel.paulander@gmail.com>
2020-08-14 23:30:53 +0200

    Token error?

--- wasm-action/entrypoint.sh
+++ wasm-action/entrypoint.sh
@@ -6,7 +6,7 @@ go build -o sandbox github.com/strosel/sandbox
 
 curl \
     -i -X PUT \
-    -H 'Authorization: token ${TOKEN}' \
+    -H 'Authorization: token ${GITHUB_TOKEN}' \
     -d "{\"path\": \"sandbox\", \
     \"message\": \"update\", \"content\": \"$(openssl base64 -A -in ./sandbox)\", \"branch\": \"pages\", \
     \"sha\": $(curl -X GET https://api.github.com/repos/strosel/sandbox/contents/sandbox | jq .sha)}" \
