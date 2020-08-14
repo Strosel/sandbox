65617a5e0155ad81375a8d4d13990b5b314982bc
axel paulander <axel.paulander@gmail.com>
2020-08-15 00:31:39 +0200

    should actually work

--- wasm-action/entrypoint.sh
+++ wasm-action/entrypoint.sh
@@ -2,19 +2,23 @@
 
 # gogio 
 
-go build -o sandbox github.com/strosel/sandbox
+REPO="sandbox"
+GITHUB_PATH="strosel/${REPO}"
+API_URL="https://api.github.com/repos/${GITHUB_PATH}/contents/${REPO}"
 
-data="{ \
+go build -o $REPO github.com/$GITHUB_PATH
+
+echo "{ \
     \"message\": \"new release\",\
     \"branch\": \"pages\",\
     \"content\": \"$(openssl base64 -A -in ./sandbox)\",\
-    \"sha\": $(curl -X GET https://api.github.com/repos/username/repo/contents/mattei.csv | jq .sha))\"
-    }"
+    \"sha\": $(curl -X GET $API_URL | jq .sha))\"
+    }" > data.txt
 
 curl \
     -X PUT \
     -H "Accept: application/vnd.github.v3+json" \
     -H "Authorization: token ${GITHUB_TOKEN}" \
-    -d "${DATA}" \
+    -d @data.txt \
     --write-out "%{http_code}" \
-    "https://api.github.com/repos/strosel/sandbox/contents/sandbox"
+    $API_URL
