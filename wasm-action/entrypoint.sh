de9cbb3ffd4f32448d2d1f4cb905c0427ccd6252
axel paulander <axel.paulander@gmail.com>
2020-08-15 00:20:53 +0200

    correct api?

--- wasm-action/entrypoint.sh
+++ wasm-action/entrypoint.sh
@@ -4,12 +4,17 @@
 
 go build -o sandbox github.com/strosel/sandbox
 
+data="{ \
+    \"message\": \"new release\",\
+    \"branch\": \"pages\",\
+    \"content\": \"$(openssl base64 -A -in ./sandbox)\",\
+    \"sha\": $(curl -X GET https://api.github.com/repos/username/repo/contents/mattei.csv | jq .sha))\"
+    }"
+
 curl \
-    -sSL \
-    -XPOST \
-    -H 'Authorization: token ${GITHUB_TOKEN}' \
-    -d '{"branch": "pages"}' \
-    --upload-file "./sandbox" \
-    --header "Content-Type:application/octet-stream" \
+    -X PUT \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: token ${GITHUB_TOKEN}" \
+    -d "${DATA}" \
     --write-out "%{http_code}" \
     "https://api.github.com/repos/strosel/sandbox/contents/sandbox"
