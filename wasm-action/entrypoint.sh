3f9143c9606d9e907d894a7387cf4f62817d13e3
axel paulander <axel.paulander@gmail.com>
2020-08-14 23:24:13 +0200

    added action test

---
+++ wasm-action/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# gogio 
+
+go build -o sandbox github.com/strosel/sandbox
+
+curl \
+    -i -X PUT \
+    -H 'Authorization: token ${TOKEN}' \
+    -d "{\"path\": \"sandbox\", \
+    \"message\": \"update\", \"content\": \"$(openssl base64 -A -in ./sandbox)\", \"branch\": \"pages\", \
+    \"sha\": $(curl -X GET https://api.github.com/repos/strosel/sandbox/contents/sandbox | jq .sha)}" \
+    "https://api.github.com/repos/strosel/sandbox/contents/mattei.csv"
